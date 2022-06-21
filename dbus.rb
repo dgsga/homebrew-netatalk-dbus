a0ae53892f5b710a03505acc0745a45f86d09cc9
dgsga <181612+dgsga@users.noreply.github.com>
2022-06-21 17:34:34 +0100

    Fix configure options for system bus on macOS

--- dbus.rb
+++ dbus.rb
@@ -12,15 +12,6 @@ class Dbus < Formula
     regex(/href=.*?dbus[._-]v?(\d+\.\d*?[02468](?:\.\d+)*)\.t/i)
   end
 
-  bottle do
-    sha256 arm64_monterey: "c9fd5fca636d95d017c791c0b5b87ec99cd3ac3ef7d194c3bb05448a52e2933a"
-    sha256 arm64_big_sur:  "26e8143ae21a90e27e9d3218c69fe7ac509379bf6adc75e0293234c366a89b35"
-    sha256 monterey:       "25e7cbac037a07c9bef1f0cf8346c6c755fa46ce1b71471e46f64f396441f734"
-    sha256 big_sur:        "76ec924fed127229906ab2e90b30ecf1a856f0f50e9ab6519cbcee1f1202632f"
-    sha256 catalina:       "8b983bdc674dff5647af3ecab82e44abacbb460db8c60435e16ae351f22bfacb"
-    sha256 x86_64_linux:   "ccf0fc79f59fa7c0ac014861c70d2df8b0c8f8c7f688d548d84b74ce998ecd38"
-  end
-
   head do
     url "https://gitlab.freedesktop.org/dbus/dbus.git"
 
@@ -34,15 +25,6 @@ class Dbus < Formula
   depends_on "xmlto" => :build
 
   uses_from_macos "expat"
-
-  on_macos do
-    # Patch applies the config templating fixed in https://bugs.freedesktop.org/show_bug.cgi?id=94494
-    # Homebrew pr/issue: 50219
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/0a8a55872e/d-bus/org.freedesktop.dbus-session.plist.osx.diff"
-      sha256 "a8aa6fe3f2d8f873ad3f683013491f5362d551bf5d4c3b469f1efbc5459a20dc"
-    end
-  end
 
   def install
     # Fix the TMPDIR to one D-Bus doesn't reject due to odd symbols
@@ -60,19 +42,39 @@ class Dbus < Formula
       "--disable-doxygen-docs",
       "--without-x",
       "--disable-tests",
+      "--disable-launchd",
+      "--with-dbus-user=root",
+      "--with-system-pid-file=/var/run/dbus.pid",
     ]
-
-    if OS.mac?
-      args << "--enable-launchd"
-      args << "--with-launchd-agent-dir=#{prefix}"
-    end
 
     system "./configure", *args
     system "make", "install"
+    system "rm", "-f", "#{etc}/dbus-1/session.conf"
+    system "rm", "-f", "#{etc}/dbus-1/system.conf"
+    system "mkdir", "-p", "#{etc}/dbus-1/system.d"
   end
 
-  def plist_name
-    "org.freedesktop.dbus-session"
+  plist_options :startup => true
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{bin}/dbus-daemon</string>
+            <string>--nofork</string>
+            <string>--system</string>
+          </array>
+          <key>KeepAlive</key>
+          <true/>
+        </dict>
+      </plist>
+    EOS
   end
 
   def post_install
